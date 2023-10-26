61c96b7c6a46f1aa2d0f9c018e188eb1b9dbf65f
siddhubolisetty <149104729+siddhubolisetty@users.noreply.github.com>
2023-10-26 13:34:41 -0500

    Add files via upload

---
+++ Auction.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+
+const CurrentRideSchema=new mongoose.Schema({
+   username:{type:String},
+   to:{type:String},
+   from:{type:String},
+   status:{type:String},
+
+   bids:[{
+      value:{type:Number},
+      bidder:{type:String},
+      vehicle:{type:String},
+      vehicaleNo:{type:Number},
+
+   }],
+   finalBidder:{type:String},
+   finalValue:{type:String},
+   Coordinates:{type:String}
+
+});
+
+const CurrentRide=mongoose.model("CurentRide",CurrentRideSchema);
+module.exports=CurrentRide;
